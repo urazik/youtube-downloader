58ef33e01c713719499eaf859a076f28d803b75d
lavr <lavrentev.srg@gmail.com>
2017-04-01 14:19:47 +0300

    convert tabs -> spaces

--- download.go
+++ download.go
@@ -1,153 +1,153 @@
 package download
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"path/filepath"
-	"strings"
-	"log"
+    "errors"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "os"
+    "path/filepath"
+    "strings"
+    "log"
 )
 
 type progress struct {
-	io.Reader
-	current int64
-	title   string
-	length  int64
+    io.Reader
+    current int64
+    title   string
+    length  int64
 }
 
 func (pr *progress) Read(p []byte) (int, error) {
-	var (
-		n   int
-		err error
-	)
+    var (
+        n   int
+        err error
+    )
 
-	if n, err = pr.Reader.Read(p); err == nil {
-		pr.current += int64(n)
-		var bytesToMB float64 = 1048576
+    if n, err = pr.Reader.Read(p); err == nil {
+        pr.current += int64(n)
+        var bytesToMB float64 = 1048576
 
-		fmt.Printf(
-			"%s   %vMB / %vMB   %v%%\r",
-			pr.title,
-			fmt.Sprintf("%.2f", float64(pr.current)/bytesToMB),
-			fmt.Sprintf("%.2f", float64(pr.length)/bytesToMB),
-			int(float64(pr.current)/float64(pr.length)*float64(100)+1),
-		)
-	}
+        fmt.Printf(
+            "%s   %vMB / %vMB   %v%%\r",
+            pr.title,
+            fmt.Sprintf("%.2f", float64(pr.current)/bytesToMB),
+            fmt.Sprintf("%.2f", float64(pr.length)/bytesToMB),
+            int(float64(pr.current)/float64(pr.length)*float64(100)+1),
+        )
+    }
 
-	return n, err
+    return n, err
 }
 
 func GetUrlsFromFile(path string) []string {
-	absPath, err := filepath.Abs(path)
-	check(err)
-	file, err := ioutil.ReadFile(absPath)
-	check(err)
+    absPath, err := filepath.Abs(path)
+    check(err)
+    file, err := ioutil.ReadFile(absPath)
+    check(err)
 
-	urls := make([]string, 0)
+    urls := make([]string, 0)
 
-	for _, s := range strings.Split(string(file), "\n") {
-		urls = append(urls, s)
-	}
+    for _, s := range strings.Split(string(file), "\n") {
+        urls = append(urls, s)
+    }
 
-	return urls
+    return urls
 }
 
 func urlParsing(u string) (string, string, error) {
-	videoUrl, err := url.Parse(u)
-	check(err)
-	m, err := url.ParseQuery(videoUrl.RawQuery)
-	check(err)
+    videoUrl, err := url.Parse(u)
+    check(err)
+    m, err := url.ParseQuery(videoUrl.RawQuery)
+    check(err)
 
-	if len(m) == 0 {
-		return "", "", errors.New("parse error")
-	}
+    if len(m) == 0 {
+        return "", "", errors.New("parse error")
+    }
 
-	getVideoInfoURL := "http://www.youtube.com/get_video_info?video_id=" + m["v"][0]
-	resp, err := http.Get(getVideoInfoURL)
-	check(err)
-	defer resp.Body.Close()
+    getVideoInfoURL := "http://www.youtube.com/get_video_info?video_id=" + m["v"][0]
+    resp, err := http.Get(getVideoInfoURL)
+    check(err)
+    defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	check(err)
+    body, err := ioutil.ReadAll(resp.Body)
+    check(err)
 
-	metadata, err := url.ParseQuery(string(body))
-	check(err)
+    metadata, err := url.ParseQuery(string(body))
+    check(err)
 
-	if metadata["status"][0] == "fail" {
-		return "", "", errors.New("parse error")
-	}
+    if metadata["status"][0] == "fail" {
+        return "", "", errors.New("parse error")
+    }
 
-	URLEncodedFmtStreamMap, err := url.ParseQuery(metadata["url_encoded_fmt_stream_map"][0])
-	check(err)
+    URLEncodedFmtStreamMap, err := url.ParseQuery(metadata["url_encoded_fmt_stream_map"][0])
+    check(err)
 
-	r := strings.NewReplacer(
-		"\\", "",
-		"/", "",
-		":", "",
-		"*", "",
-		"?", "",
-		"\"", "",
-		"<", "",
-		">", "",
-		"|", "",
-	)
-	title := r.Replace(metadata["title"][0])
+    r := strings.NewReplacer(
+        "\\", "",
+        "/", "",
+        ":", "",
+        "*", "",
+        "?", "",
+        "\"", "",
+        "<", "",
+        ">", "",
+        "|", "",
+    )
+    title := r.Replace(metadata["title"][0])
 
-	return URLEncodedFmtStreamMap["url"][0], title, nil
+    return URLEncodedFmtStreamMap["url"][0], title, nil
 }
 
 func createFile(dir, title string) *os.File {
-	if strings.LastIndex(dir, string(filepath.Separator)) != len(dir)-1 {
-		dir = dir + string(filepath.Separator)
-	}
+    if strings.LastIndex(dir, string(filepath.Separator)) != len(dir)-1 {
+        dir = dir + string(filepath.Separator)
+    }
 
-	path := dir + title + ".mp4"
+    path := dir + title + ".mp4"
 
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
-		err := os.Remove(path)
-		check(err)
-	}
+    if _, err := os.Stat(path); !os.IsNotExist(err) {
+        err := os.Remove(path)
+        check(err)
+    }
 
-	out, err := os.Create(path)
-	check(err)
+    out, err := os.Create(path)
+    check(err)
 
-	return out
+    return out
 }
 
 func Download(urls []string, dir string) {
-	fmt.Print("Start download\n")
+    fmt.Print("Start download\n")
 
-	for _, u := range urls {
-		fmt.Println()
+    for _, u := range urls {
+        fmt.Println()
 
-		dUrl, title, err := urlParsing(u)
-		check(err)
+        dUrl, title, err := urlParsing(u)
+        check(err)
 
-		out := createFile(dir, title)
-		defer out.Close()
+        out := createFile(dir, title)
+        defer out.Close()
 
-		resp, err := http.Get(dUrl)
-		check(err)
-		defer resp.Body.Close()
+        resp, err := http.Get(dUrl)
+        check(err)
+        defer resp.Body.Close()
 
-		_, err = io.Copy(out, &progress{
-			Reader: resp.Body,
-			title:  title,
-			length: resp.ContentLength,
-		})
-		check(err)
-	}
+        _, err = io.Copy(out, &progress{
+            Reader: resp.Body,
+            title:  title,
+            length: resp.ContentLength,
+        })
+        check(err)
+    }
 
-	fmt.Print("\n\nDownload end")
+    fmt.Print("\n\nDownload end")
 }
 
 func check(err error) {
-	if err != nil {
-		log.Fatal("Oops, some error! Check the urls or file path.", err)
-	}
+    if err != nil {
+        log.Fatal("Oops, some error! Check the urls or file path.", err)
+    }
 }
